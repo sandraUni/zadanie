21386e08a16e4bab1cd5c4b9840f87db82c6865f
122137 <122137@student.uni.opole.pl>
2020-12-09 19:30:58 +0100

    added Window_Loaded in BaseSQL

--- zadanie/BaseSQL.xaml.cs
+++ zadanie/BaseSQL.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace zadanie
 {
@@ -22,6 +23,13 @@ namespace zadanie
         public BaseSQL()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Base.readBase();
+
+            myDataGrid.ItemsSource = Base.dataTable.DefaultView;
         }
     }
 }
